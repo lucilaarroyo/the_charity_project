db8286f1abbe6b9eed407b48ec83832654c5ecbe
Lucila Arroyo <lucilaa@uci.edu>
2021-05-18 14:41:03 -0700

    message

---
+++ settings.py
@@ -0,0 +1,176 @@
+import os
+from os import environ
+import dj_database_url
+import otree.settings
+
+
+# if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
+# in SESSION_CONFIGS, except those that explicitly override it.
+# the session config can be accessed from methods in your apps as self.session.config,
+# e.g. self.session.config['participation_fee']
+
+SESSION_CONFIG_DEFAULTS = {
+    'real_world_currency_per_point': 1.00,
+    'participation_fee': 0.00,
+    'doc': "",
+}
+
+SESSION_CONFIGS = [
+
+    {
+        'name': 'Comp',
+        'display_name': "Comp",
+        'num_demo_participants': 2,
+        'app_sequence': ['Comp'],
+    },
+
+    # {
+    #     'name': 'accom_dynam',
+    #     'display_name': "Accom dy",
+    #     'num_demo_participants': 2,
+    #     'app_sequence': ['accom_dynam'],
+    # },
+
+    # {
+    #     'name': 'accom_dynam_single',
+    #     'display_name': "Accom dy single",
+    #     'num_demo_participants': 2,
+    #     'app_sequence': ['accom_dynam_single'],
+    # },
+
+    # {
+    #     'name': 'Stag_Hunt',
+    #     'display_name': "Stag Hunt",
+    #     'num_demo_participants': 2,
+    #     'app_sequence': ['Stag_Hunt'],
+    # },
+
+    # {
+    #     'name': 'dictator',
+    #     'display_name': "Dictator Game",
+    #     'num_demo_participants': 2,
+    #     'app_sequence': ['dictator', 'payment_info'],
+    # },
+
+    # {
+    #     'name': 'survey',
+    #     'display_name': "Survey",
+    #     'num_demo_participants': 1,
+    #     'app_sequence': ['survey', 'payment_info'],
+    # },
+
+    # {
+    #     'name': 'surveytwo',
+    #     'display_name': "Surveytwo",
+    #     'num_demo_participants': 1,
+    #     'app_sequence': ['surveytwo',],
+    # },
+
+
+
+]
+# see the end of this file for the inactive session configs
+
+
+# ISO-639 code
+# for example: de, fr, ja, ko, zh-hans
+LANGUAGE_CODE = 'en'
+
+# e.g. EUR, GBP, CNY, JPY
+REAL_WORLD_CURRENCY_CODE = 'USD'
+USE_POINTS = False
+
+ROOMS = [
+    {
+        'name': 'econ101',
+        'display_name': 'Econ 101 class',
+        'participant_label_file': '_rooms/econ101.txt',
+    },
+    {
+        'name': 'live_demo',
+        'display_name': 'Room for live demo (no participant labels)',
+    },
+]
+
+
+# AUTH_LEVEL:
+# this setting controls which parts of your site are freely accessible,
+# and which are password protected:
+# - If it's not set (the default), then the whole site is freely accessible.
+# - If you are launching a study and want visitors to only be able to
+#   play your app if you provided them with a start link, set it to STUDY.
+# - If you would like to put your site online in public demo mode where
+#   anybody can play a demo version of your game, but not access the rest
+#   of the admin interface, set it to DEMO.
+
+# for flexibility, you can set it in the environment variable OTREE_AUTH_LEVEL
+AUTH_LEVEL = environ.get('OTREE_AUTH_LEVEL')
+
+ADMIN_USERNAME = 'admin'
+# for security, best to set admin password in an environment variable
+ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
+
+
+# Consider '', None, and '0' to be empty/false
+DEBUG = (environ.get('OTREE_PRODUCTION') in {None, '', '0'})
+
+DEMO_PAGE_INTRO_HTML = """
+<ul>
+    <li>
+        <a href="https://github.com/oTree-org/otree" target="_blank">
+            oTree on GitHub
+        </a>.
+    </li>
+    <li>
+        <a href="http://www.otree.org/" target="_blank">
+            oTree homepage
+        </a>.
+    </li>
+</ul>
+<p>
+    Here are various games implemented with oTree. These games are all open
+    source, and you can modify them as you wish.
+</p>
+"""
+
+# don't share this with anybody.
+SECRET_KEY = 'k+^g(01t_&m-%x)nq$2mtu)$n$0di7k*qf)hk9ya@-%d4j^t9v'
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         # Rather than hardcoding the DB parameters here,
+#         # it's recommended to set the DATABASE_URL environment variable.
+#         # This will allow you to use SQLite locally, and postgres/mysql
+#         # on the server
+#         # Examples:
+#         # export DATABASE_URL=postgres://USER:PASSWORD@HOST:PORT/NAME
+#         # export DATABASE_URL=mysql://USER:PASSWORD@HOST:PORT/NAME
+#
+#         # fall back to SQLite if the DATABASE_URL env var is missing
+#         default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+#     )
+# }
+
+
+# if an app is included in SESSION_CONFIGS, you don't need to list it here
+INSTALLED_APPS = ['otree']
+
+# inactive session configs
+
+
+### {
+###     'name': 'real_effort',
+###     'display_name': "Real-effort transcription task",
+###     'num_demo_participants': 1,
+###     'app_sequence': [
+###         'real_effort',
+###     ],
+### },
+### {
+
+
+
+otree.settings.augment_settings(globals())
